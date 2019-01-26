d4b5f7712bcbc8f0a518c46d864f1ee3fb50dde1
luoguibin <luoguibin@formail.com>
2019-01-26 14:57:48 +0800

    Add: the .env file

--- src/main.js
+++ src/main.js
@@ -7,8 +7,14 @@ import plugins from './plugins/index'
 Vue.config.productionTip = false
 Vue.use(plugins)
 
+let date = "2019-01-26";
+if (process.env.VUE_APP_SGHEN_MODE === 'prod')
+  date = new Date().toUTCString();
+console.log(date)
+
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
