76ff98bcfe3727b923437ce0057da0bf0b67e954
luoguibin <luoguibin@formail.com>
2019-02-16 16:00:46 +0800

    Fix: p

--- src2/main.js
+++ src2/main.js
@@ -1,1 +1,24 @@
-console.log("src2 main.js")
+const _obj = {
+    id: 123456789,
+    name: "123"
+}
+
+const obj = {
+    id: 123456789,
+    name: "123"
+}
+
+Object.defineProperties(obj, {
+    id: {
+        get: function() {
+            console.log("obj get id")
+            return _obj.id
+        },
+        set: function(v) {
+            console.log("obj set id=" + v)
+            _obj.id = v;
+        }
+    }
+})
+
+window.obj = obj;
--- src3/App.vue
+++ src3/App.vue
@@ -9,13 +9,13 @@
 
         <div style="margin-top: 50px;">
             <p>
-                <span class="bold">计算属性</span> 是基于它们的依赖进行缓存的。
-            </p>
+                <span class="bold">计算属性</span> 是基于它们的依赖进行缓存的。</p>
             <p>
-                <span class="bold">侦听器watch</span> 可响应数据的变化,当需要在数据变化时执行异步或开销较大的操作时，次方法最有用。
-            </p>
+                <span class="bold">侦听器watch</span> 可响应数据的变化,当需要在数据变化时执行异步或开销较大的操作时，次方法最有用。</p>
             <p>用
                 <span class="bold">key</span> 管理可复用的元素,Vue 会尽可能高效地渲染元素，通常会复用已有元素而不是从头开始渲染。</p>
+            <p>Vue 不能检测到对象属性的添加或删除。Vue 不允许在已经创建的实例上动态添加新的根级响应式属性 (root-level reactive property)。然而它可以使用 Vue.set(object, key, value) 方法将响应属性添加到嵌套的对象上。添加多个属性可用: this.someObject = Object.assign({}, this.someObject, { a: 1, b: 2 })</p>
+            <p>Vue 异步执行 DOM 更新。只要观察到数据变化，Vue 将开启一个队列，并缓冲在同一事件循环中发生的所有数据改变。如果同一个 watcher 被多次触发，只会被推入到队列中一次。this.$nextTick()自动绑定当前vue实例，而Vue.nextTick()则是全局，需要使用箭头函数。</p>
         </div>
 
         <div>
@@ -33,11 +33,14 @@ export default {
   },
   mounted() {
     window.vApp = this;
-    this.$root.getDynamicComponent('dyna0').then(o => {
+    this.$root
+      .getDynamicComponent("dyna0")
+      .then(o => {
         Vue.component("dyna0", o.default);
-    }).catch(e => {
+      })
+      .catch(e => {
         // console.log("dyna0 load error")
-    });
+      });
   },
   methods: {}
 };
@@ -46,5 +49,9 @@ export default {
 <style lang="css" scoped>
 .bold {
   font-weight: bold;
+}
+
+p {
+    text-indent: 2em;
 }
 </style>
