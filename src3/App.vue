cb2cf28dcaf4de36cdd86a27710b97cd95c2e71f
luoguibin <luoguibin@formail.com>
2019-02-18 15:29:48 +0800

    Add: the vue test

--- src3/App.vue
+++ src3/App.vue
@@ -16,20 +16,29 @@
                 <span class="bold">key</span> 管理可复用的元素,Vue 会尽可能高效地渲染元素，通常会复用已有元素而不是从头开始渲染。</p>
             <p>Vue 不能检测到对象属性的添加或删除。Vue 不允许在已经创建的实例上动态添加新的根级响应式属性 (root-level reactive property)。然而它可以使用 Vue.set(object, key, value) 方法将响应属性添加到嵌套的对象上。添加多个属性可用: this.someObject = Object.assign({}, this.someObject, { a: 1, b: 2 })</p>
             <p>Vue 异步执行 DOM 更新。只要观察到数据变化，Vue 将开启一个队列，并缓冲在同一事件循环中发生的所有数据改变。如果同一个 watcher 被多次触发，只会被推入到队列中一次。this.$nextTick()自动绑定当前vue实例，而Vue.nextTick()则是全局，需要使用箭头函数。</p>
+            <p>使用 $parent 属性无法很好的扩展到更深层级的嵌套组件上。这也是依赖注入的用武之地，它用到了两个新的实例选项：provide 和 inject。</p>
         </div>
 
         <div>
+            <g-hello></g-hello>
+            <test-render>
+                day day up
+            </test-render>
         </div>
     </div>
 </template>
 
 <script>
 import Vue from "vue";
+import TestRender from "./components/test-render";
 
 export default {
+  name: "App",
   data() {
-    return {
-    };
+    return {};
+  },
+  components: {
+    "test-render": TestRender
   },
   mounted() {
     window.vApp = this;
@@ -42,6 +51,13 @@ export default {
         // console.log("dyna0 load error")
       });
   },
+  provide() {
+    return {
+      getApp: () => {
+        return this;
+      }
+    };
+  },
   methods: {}
 };
 </script>
@@ -52,6 +68,6 @@ export default {
 }
 
 p {
-    text-indent: 2em;
+  text-indent: 2em;
 }
 </style>
---
+++ src3/components/global/g-hello.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      txt: "g-hello"
+    };
+  },
+  inject: {
+    getApp2: "getApp"
+  },
+  // inject: ['getApp']
+  mounted() {
+    console.log(this.getApp2().$options.name);
+
+    let obj = {};
+    this.$once("hook:beforeDestroy", () => {
+      obj = null;
+    });
+  }
+};
+</script>
+
+<style>
+</style>
