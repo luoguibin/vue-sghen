b81d4367c34fbc8a442934818f0eaeda9e9324da
luoguibin <luoguibin@formail.com>
2019-03-29 15:11:46 +0800

    Fix: the .gitignore

--- buildToDocker.sh
+++ buildToDocker.sh
@@ -1,5 +1,5 @@
 sudo docker build -t vue-sghen:1.2.3 .
-sudo docker service update --image vue-sghen:latest vue-sghen-service
+sudo docker service update --image vue-sghen:1.2.3 vue-sghen-service
 
 # docker images // 查看所有镜像
 # sudo docker rmi $(docker images -f "dangling=true" -q) // 删除构建镜像时暂时的临时的空名字镜像
