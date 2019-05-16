dd808daf87699b7399422813b4f6c2e24328eec6
guibinluo <945654877@qq.com>
2019-05-16 15:06:22 +0800

    Add: the canvas-nest

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app" ref="canvas">
     <router-view/>
   </div>
 </template>
 
 <script>
+import CanvasNest from "canvas-nest.js";
+
 export default {
   name: "app",
   mounted() {
     window.app = this;
+
+    // Using config rendering effect at 'element'.
+    const cn = new CanvasNest(this.$refs.canvas, {
+      color: "#148acf",
+      count: 88,
+      zIndex: 0
+    });
+    this.$once("destroy", () => {
+      cn.destroy();
+    });
   }
 };
 </script>
@@ -27,7 +39,7 @@ body {
 #app {
   height: 100%;
   font-family: "SimSun";
-  background-image: url('./assets/img/qiangzhi01.gif');
+  background-image: url("./assets/img/qiangzhi01.gif");
   background-repeat: repeat;
 }
 </style>
