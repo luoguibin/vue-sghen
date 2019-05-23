f5e574ada609503a7e0b6f3ef5969ec2716cbe0a
guibinluo <945654877@qq.com>
2019-05-23 18:32:03 +0800

    Temp: the user name udpate

--- src/views/home-peotry.vue
+++ src/views/home-peotry.vue
@@ -16,39 +16,52 @@
         </el-dropdown-menu>
       </el-dropdown>
 
-      <el-dialog title="个人信息" :visible.sync="showUser" v-if="showUserInfo">
+      <el-dialog title="个人信息" :visible.sync="showUser" @open="onUserInfoDialogOpen">
         <el-form label-width="60px">
           <el-form-item label="ID" v-if="showUserInfo.token">
-            <el-input readonly v-model="showUserInfo.id"></el-input>
+            <el-input disabled="" v-model="showUserInfo.id"></el-input>
           </el-form-item>
 
           <el-form-item label="昵称">
-            <el-input readonly v-model="showUserInfo.name"></el-input>
+            <el-input :disabled="showUserInfo.token ? false : true" v-model="showUserInfo.name"></el-input>
           </el-form-item>
 
           <el-form-item label="头像">
             <img :src="showUserInfo.iconUrl" style="max-width: 50px; vertical-align: top;">
-            <el-upload
-              v-if="showUserInfo.token"
-              class="upload-icon"
-              ref="upload"
-              :action="baseUrl + '/v1/upload'"
-              :data="{'pathType': 'icon', 'token': userInfo.token}"
-              :show-file-list="false"
-              :auto-upload="true"
-              :on-success="onIconUploadSuccess"
-            >
-              <el-button slot="trigger" size="small" type="primary">选取头像</el-button>
-              <!-- <el-button
-                style="margin-left: 10px;"
-                size="small"
-                type="success"
-                @click="submitUpload"
-              >上传</el-button>
-              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
-            </el-upload>
+            <span v-if="showUserInfo.token">
+              <el-button @click="onClickIconUpdate">更换</el-button>
+              <input
+                type="file"
+                ref="iconInput"
+                accept="image/*"
+                @change="onIconChange"
+                v-show="false"
+              >
+            </span>
+          </el-form-item>
+
+          <el-form-item v-if="showUserInfo.token">
+            <el-button type="primary" @click="onUpdateUserInfo">更新信息</el-button>
           </el-form-item>
         </el-form>
+      </el-dialog>
+
+      <el-dialog title="头像更新" :visible.sync="iconDialogVisible">
+        <vue-cropper
+          ref="cropper"
+          class="cropper"
+          :img="option.img"
+          :outputType="option.outputType"
+          :info="option.info"
+          :canMoveBox="option.canMoveBox"
+          :fixedBox="option.fixedBox"
+          :original="option.original"
+          :autoCrop="option.autoCrop"
+          :autoCropWidth="option.autoCropWidth"
+          :autoCropHeight="option.autoCropHeight"
+          :maxImgSize="option.maxImgSize"
+        ></vue-cropper>
+        <el-button @click="onSubmitCropper">更新</el-button>
       </el-dialog>
 
       <peotry-create :showCreate="showCreate" :peotry="updatePeotry" @on-close="onPeotryClose"></peotry-create>
@@ -105,13 +118,15 @@
 import { mapState, mapActions } from "vuex";
 import Peotry from "@/components/peotry/peotry";
 import PeotryCreate from "@/components/peotry/peotry-create";
+import { VueCropper } from "vue-cropper";
 import {
   updateUser,
   queryUsers,
   queryPeotries,
   deletePeotry,
   createComment,
-  deleteComment
+  deleteComment,
+  uploadFiles
 } from "../api";
 import { baseUrl } from "../api/config";
 import { resetUserIconUrl } from "@/common/utils/icon-util";
@@ -120,7 +135,8 @@ export default {
   name: "HomePeotry",
   components: {
     peotry: Peotry,
-    "peotry-create": PeotryCreate
+    "peotry-create": PeotryCreate,
+    "vue-cropper": VueCropper
   },
   data() {
     return {
@@ -147,7 +163,21 @@ export default {
       curPage: 1,
       totalPage: 1,
       totalCount: 0,
-      peotries: []
+      peotries: [],
+
+      iconDialogVisible: false,
+      option: {
+        img: "",
+        original: true, // 上传图片按照原始比例渲染
+        outputType: "png",
+        fixedBox: true, // 固定截图框大小 不允许改变
+        canMoveBox: false,
+        autoCrop: true,
+        autoCropWidth: 128, // 只有自动截图开启 宽度高度才生效
+        autoCropHeight: 128,
+        info: false, // 隐藏裁剪框大小信息
+        maxImgSize: 300
+      }
     };
   },
   provide() {
@@ -222,24 +252,89 @@ export default {
     onShowLogin() {
       this.showLogin();
     },
-    submitUpload() {
-      this.$refs.upload.submit();
-    },
-    onIconUploadSuccess(response, file, fileList) {
-      if (response.code !== 1000) return;
 
-      updateUser({ iconUrl: response.data[0], uId: this.userInfo.id }).then(
-        resp => {
-          if (resp.data.code === 1000) {
-            this.$appTip("更新头像成功");
-            const info = { ...this.userInfo, iconUrl: response.data[0] };
-            resetUserIconUrl(info);
-            this.setUserInfo(info);
-          } else {
-            this.$appTip(resp.data.msg);
-          }
+    onUserInfoDialogOpen() {
+      this.oldShowUserInfo = JSON.parse(JSON.stringify(this.showUserInfo))
+    },
+
+    onClickIconUpdate() {
+      this.$refs.iconInput.click();
+    },
+    /**
+     * 打开图片选择
+     */
+    onIconChange(e) {
+      const file = e.target.files && e.target.files[0];
+      if (!file) {
+        return;
+      }
+
+      const reader = new FileReader(reader);
+      reader.onload = e => {
+        this.option.img = reader.result;
+        this.iconDialogVisible = true;
+      };
+      reader.readAsDataURL(file);
+    },
+    onSubmitCropper() {
+      this.$refs.cropper.getCropBlob(data => {
+        const file = new File([data], "example.png", {
+          type: "image/png",
+          lastModified: Date.now()
+        });
+        const formData = new FormData();
+        formData.append("file", file);
+
+        uploadFiles({ pathType: "normal" }, formData)
+          .then(resp => {
+            if (resp.data.code !== 1000) {
+              this.$appTip(resp.data.msg);
+              return;
+            }
+
+            const iconUrl = resp.data.data[0];
+            updateUser({
+              iconUrl,
+              uId: this.userInfo.id
+            }).then(resp => {
+              if (resp.data.code === 1000) {
+                this.$appTip("更新头像成功");
+                const info = { ...this.userInfo, iconUrl: iconUrl };
+                resetUserIconUrl(info);
+                this.setUserInfo(info);
+                this.iconDialogVisible = false;
+              } else {
+                this.$appTip(resp.data.msg);
+              }
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+    },
+    onUpdateUserInfo() {
+      if (this.oldShowUserInfo.name !== this.showUserInfo.name) {
+        if (this.showUserInfo.name.length) {
+          updateUser({
+              name: this.showUserInfo.name,
+              uId: this.userInfo.id
+            }).then(resp => {
+              if (resp.data.code === 1000) {
+                this.$appTip("更新个人信息成功");
+                const info = { ...this.userInfo, name: this.showUserInfo.name };
+                this.setUserInfo(info);
+                this.showUser = false;
+              } else {
+                this.$appTip(resp.data.msg);
+              }
+            });
+        } else {
+          this.$message("名字不能为空")
         }
-      );
+      } else {
+        this.$message("个人信息未发生变化")
+      }
     },
     updatePeotriesData(datas) {
       const idsSet = new Set();
@@ -457,6 +552,12 @@ export default {
         background-color: rgba(0, 0, 0, 0.5);
       }
     }
+  }
+
+  .cropper {
+    display: inline-block;
+    width: 300px;
+    height: 300px;
   }
 }
 </style>
