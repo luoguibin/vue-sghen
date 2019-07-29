5f3c7ef68f7b1923e00d1eb2a2dcc1db76d1c016
guibinluo <945654877@qq.com>
2019-07-29 10:10:43 +0800

    [*]删除部分无用代码

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-import './style/element-ui.css'
-// Vue.use(ElementUI)
-
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
--- src/style/build/build.bat
+++
@@ -1,7 +0,0 @@
-:: vue build .\theme-default.vue
-cd .\dist\css\
-ren app.*.css theme-default.css
-cd ..\..\
-copy .\dist\css\theme-default.css ..\
-rd /s /q .\node_modules
-rd /s /q .\dist
