1549ce316538605ea01ec3fab7f6bb6bdc310dd7
Sghen <945654877@qq.com>
2019-09-01 22:56:37 +0800

    [*]添加云服务测试

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.NODE_ENV === "production" ? "http://localhost:8088" : "http://127.0.0.1:8088";
-export const wsUrl = process.env.NODE_ENV === "production" ? "ws://127.0.0.1:8088/ws" : "ws://127.0.0.1:8088/ws";
+export const baseUrl = process.env.NODE_ENV === "production" ? "http://49.235.91.138:8088" : "http://127.0.0.1:8088";
+export const wsUrl = process.env.NODE_ENV === "production" ? "ws://49.235.91.138:8088/ws" : "ws://127.0.0.1:8088/ws";
