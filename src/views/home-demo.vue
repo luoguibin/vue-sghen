b38799218d7ae1676502c07ec0562bdc38bfa5e9
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2019-10-21 17:16:54 +0800

    [*]修复demo组件加载

--- src/views/home-demo.vue
+++ src/views/home-demo.vue
@@ -1,9 +1,10 @@
 <template>
-  <component v-if="isCompReady" :is="compName"></component>
+  <component v-if="compState !== 0" :is="compState === 1 ? compName : 'home-error'"></component>
 </template>
 
 <script>
 import Vue from "vue";
+import HomeError from "./home-error";
 
 const getDemoComponent = function (name) {
     return import(/* webpackChunkName: "[request]" */"@/components/" + name);
@@ -17,9 +18,12 @@ export default {
       default: "list"
     }
   },
+  components: {
+    HomeError
+  },
   data() {
     return {
-      isCompReady: false
+      compState: 0
     };
   },
 
@@ -38,15 +42,16 @@ export default {
   methods: {
     initComp() {
       this.$NProgress.start();
-      this.isCompReady = false;
+      this.compState = 0;
 
       getDemoComponent(this.compName)
         .then(o => {
           Vue.component(this.compName, o.default);
-          this.isCompReady = true;
+          this.compState = 1;
         })
         .catch(err => {
           this.$message.error({ message: err });
+          this.compState = -1;
         })
         .finally(() => {
           this.$NProgress.done();
