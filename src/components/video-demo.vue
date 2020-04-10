57a0360ca593d3ebe6b8ea2b00ba7ed0b3c9a02f
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-04-10 10:12:22 +0800

    [*]测试video

--- src/components/video-demo.vue
+++ src/components/video-demo.vue
@@ -77,6 +77,33 @@ export default {
   updated() {},
 
   methods: {
+    onClickTitle() {},
+    //全屏
+    fullScreen() {
+      const element = this.$refs.videoWrapper;
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      }
+    },
+
+    //退出全屏
+    exitFullscreen() {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    },
     togglePlay() {
       const video = this.$refs.video;
       // console.log(video.readyState, video.paused)
