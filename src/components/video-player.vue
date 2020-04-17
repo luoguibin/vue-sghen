e20d14a03cee826ad77c4ba126f0ddee3188d370
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-04-17 17:34:53 +0800

    [*]测试视频

--- src/components/video-player.vue
+++ src/components/video-player.vue
@@ -6,11 +6,17 @@
         <p class="video-name" @click="onClickTitle">{{videoName}}</p>
       </div>
       <div>
-        <input v-model="videoURL0" style="width: calc(100% - 60px);line-height: 25px; display: inline-block; padding: 0 10px; box-sizing: border-box;" />
-        <button @click="onResetLoad()" style="line-height: 25px; width: 60px;box-sizing: border-box;">加载</button>
+        <input
+          v-model="videoURL0"
+          style="width: calc(100% - 60px);line-height: 25px; display: inline-block; padding: 0 10px; box-sizing: border-box;"
+        />
+        <button
+          @click="onResetLoad()"
+          style="line-height: 25px; width: 60px;box-sizing: border-box;"
+        >加载</button>
       </div>
     </div>
-    
+
     <div ref="wrapper" :class="{'video-player-wrapper': true, 'video-hidden': !videoReady }">
       <video
         ref="video"
@@ -26,15 +32,31 @@
         @play="hanlePlay"
         @playing="handlePlaying"
         @error="handleError"
+        @loadstart="handleLoadStart"
+        @durationchange="handleDurationChange"
+        @loadedmetadata="handleLoadedMetadata"
+        @progress="handleProgress"
+        @canplaythrough="handlePlayThrough"
       ></video>
       <div class="video-player-poster" v-show="posterVisible">
-        <img :class="{'poster': true, 'img-hidden': !isPosterLoaded}" :src="videoPosterURL" @error="isPosterLoaded = false" @load="isPosterLoaded = true" />
-        <img class="play" v-show="videoReady" @click="onInitPlay" src="@/assets/image/audio/audio_pause.png" />
+        <img
+          :class="{'poster': true, 'img-hidden': !isPosterLoaded}"
+          :src="videoPosterURL"
+          @error="isPosterLoaded = false"
+          @load="isPosterLoaded = true"
+        />
+        <img
+          class="play"
+          v-show="videoReady"
+          @click="onInitPlay"
+          src="@/assets/image/audio/audio_pause.png"
+        />
       </div>
     </div>
 
-     <div class="video-player-tip" v-show="tipVisible">
-      <p>加载失败，请
+    <div class="video-player-tip" v-show="tipVisible">
+      <p>
+        加载失败，请
         <span @click="onReload">重新加载</span>
       </p>
     </div>
@@ -47,57 +69,66 @@
 
 <script>
 export default {
-  name: 'VideoPlayer',
+  name: "VideoPlayer",
 
-  data () {
+  data() {
     return {
       videoReady: false,
-      videoName: 'big_buck_bunny.mp4',
-      videoURL: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-      videoURL0: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-      videoPosterURL: 'http://218.104.127.194:2414/storageWeb/servlet/GetFileByURLServlet?root=/mnt/D999&fileid=mz74bda2ec99358cab89c7625ff8c28e6c.mp4&ct=3&type=1&code=A16BCA895B99F451D0F77AFC92EC9339033717F750518E923756F3AC15356D28&account=MTU2MjUwNDU5ODQ=&p=0&ui=TT11jJpQI00x&ci=TT11jJpQI00x0042020040816524000h&cn=mov_bbb&oprChannel=10000000&dom=D888',
+      videoName: "big_buck_bunny.mp4",
+      videoURL: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
+      videoURL0: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
+      videoPosterURL:
+        "http://218.104.127.194:2414/storageWeb/servlet/GetFileByURLServlet?root=/mnt/D999&fileid=mz74bda2ec99358cab89c7625ff8c28e6c.mp4&ct=3&type=1&code=A16BCA895B99F451D0F77AFC92EC9339033717F750518E923756F3AC15356D28&account=MTU2MjUwNDU5ODQ=&p=0&ui=TT11jJpQI00x&ci=TT11jJpQI00x0042020040816524000h&cn=mov_bbb&oprChannel=10000000&dom=D888",
       tipVisible: false,
       posterVisible: true,
       isPosterLoaded: true,
       hasUserAction: false,
       isFullScreen: false,
-      logMsg: 'create\n'
-    }
+      logMsg: "create\n"
+    };
   },
 
-  mounted () {
-    window.videoPlayer = this
+  mounted() {
+    window.videoPlayer = this;
     // this.$loading()
-    this.$refs.video.load()
-  
-    this.$el.parentElement.style.padding = '0'
+    this.$refs.video.load();
+
+    this.$el.parentElement.style.padding = "0";
   },
 
   methods: {
-    goBack () {
+    goBack() {
       if (history.length > 1) {
-        this.$router.go(-1)
+        this.$router.go(-1);
       } else {
-        this.$router.push('/main')
+        this.$router.push("/main");
       }
     },
-    onInitPlay (e) {
-      const video = this.$refs.video
-      video.play()
-      this.posterVisible = false
-      this.hasUserAction = !!e
+    onInitPlay(e) {
+      const video = this.$refs.video;
+      video.play();
+      this.posterVisible = false;
+      this.hasUserAction = !!e;
 
       // 进入全屏，设置状态
-      video.addEventListener('x5videoenterfullscreen', () => {
-        this.logMsg += 'x5videoenterfullscreen\n'
-        this.isFullScreen = true
-      }, false)
+      video.addEventListener(
+        "x5videoenterfullscreen",
+        () => {
+          this.logMsg += "x5videoenterfullscreen\n";
+          this.isFullScreen = true;
+        },
+        false
+      );
 
       // 退出全屏时，清空状态
-      video.addEventListener('x5videoexitfullscreen', () => {
-        this.logMsg += 'x5videoexitfullscreen\n'
-        this.isFullScreen = false
-      }, false)
+      video.addEventListener(
+        "x5videoexitfullscreen",
+        () => {
+          this.logMsg += "x5videoexitfullscreen\n";
+          this.isFullScreen = false;
+        },
+        false
+      );
 
       // this.$el.requestFullscreen().then(resp => {
       //   console.log(resp)
@@ -106,75 +137,93 @@ export default {
       // })
     },
 
-    onResetLoad (e) {
-      this.videoURL = this.videoURL0
-      this.onReload(e)
+    onResetLoad(e) {
+      this.videoURL = this.videoURL0;
+      this.onReload(e);
     },
 
-    onReload (e) {
+    onReload(e) {
       if (!this.isOnline()) {
-        alert('网络异常，请稍后重试')
-        return
+        alert("网络异常，请稍后重试");
+        return;
       }
-      this.tipVisible = false
-      this.posterVisible = true
-      this.videoReady = false
+      this.tipVisible = false;
+      this.posterVisible = true;
+      this.videoReady = false;
       // this.$loading()
-      this.hasUserAction = !!e
-      this.$refs.video.load()
+      this.hasUserAction = !!e;
+      this.$refs.video.load();
     },
 
-    handleWaiting () {
+    handleLoadStart() {
       // console.log('handleWaiting', e)
-      this.logMsg += 'waiting\n'
+      this.logMsg += "loadstart\n";
     },
-    handleLoadedData () {
+    handleDurationChange() {
+      // console.log('handleWaiting', e)
+      this.logMsg += "duration change\n";
+    },
+    handleLoadedMetadata() {
+      // console.log('handleWaiting', e)
+      this.logMsg += "loadedmetadata\n";
+    },
+    handleProgress() {
+      // console.log('handleWaiting', e)
+      this.logMsg += "pregress\n";
+    },
+    handlePlayThrough() {
+      // console.log('handleWaiting', e)
+      this.logMsg += "playthrough\n";
+    },
+    handleWaiting() {
+      // console.log('handleWaiting', e)
+      this.logMsg += "waiting\n";
+    },
+    handleLoadedData() {
       // console.log('handleLoadedData', e)
-      this.logMsg += 'loadeddata\n'
+      this.logMsg += "loadeddata\n";
     },
-    hanldeCanPlay () {
+    hanldeCanPlay() {
       // console.log('hanldeCanPlay', e)
-      this.logMsg += 'canplay\n'
-      const video = this.$refs.video
+      this.logMsg += "canplay\n";
+      const video = this.$refs.video;
       if (!this.videoReady) {
-        video.setAttribute('controls', true)
+        video.setAttribute("controls", true);
         // this.$loading.hide()
-        this.videoReady = true
+        this.videoReady = true;
       }
       if (video.paused && this.hasUserAction) {
-        video.play()
+        video.play();
       }
     },
-    hanlePlay () {
+    hanlePlay() {
       // console.log('hanlePlay', e)
-      this.logMsg += 'play\n'
+      this.logMsg += "play\n";
     },
-    handlePlaying () {
+    handlePlaying() {
       // console.log('handlePlaying', e)
-      this.logMsg += 'playing\n'
+      this.logMsg += "playing\n";
     },
-    handleError (e) {
-      const error = e.target.error
-      this.logMsg += `error ${JSON.stringify(error)}\n`
+    handleError(e) {
+      const error = e.target.error;
+      this.logMsg += `error ${JSON.stringify(error)}\n`;
       // 1 = MEDIA_ERR_ABORTED - 取回过程被用户中止
       // 2 = MEDIA_ERR_NETWORK - 当下载时发生错误
       // 3 = MEDIA_ERR_DECODE - 当解码时发生错误
       // 4 = MEDIA_ERR_SRC_NOT_SUPPORTED - 不支持音频/视频
       if (error.code > 1) {
-        if (!this.isOnline()) alert('网络异常，请稍后重试')
-        this.tipVisible = true
+        if (!this.isOnline()) alert("网络异常，请稍后重试");
+        this.tipVisible = true;
       }
       // this.$loading.hide()
       // console.log('handleError', error)
     },
-    onClickTitle () {
-
-    },
-    isOnline () {
-      return navigator.onLine
+    onClickTitle() {},
+    isOnline() {
+      return navigator.onLine;
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -277,10 +326,10 @@ export default {
       left: 50%;
       transform: translate(-50%, -50%);
       text-align: center;
-      color:rgba(255, 255, 255, 0.8);
+      color: rgba(255, 255, 255, 0.8);
       font-size: 1.2rem;
       span {
-        color:rgba(49, 129, 249, 0.8);
+        color: rgba(49, 129, 249, 0.8);
       }
     }
   }
